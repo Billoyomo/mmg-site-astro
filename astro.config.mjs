4d6d4d77cd28acef62cd449dc5ff204d1e466012
schege882 <schege882@gmail.com>
2026-07-25 16:05:51 +0300

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import remarkBasePath from './src/plugins/remark-base-path.mjs';
 
-const BASE = '/mmg-site-astro';
+const BASE = '/mmg-site-astro/';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://schege882.github.io',
   base: BASE,
   markdown: {
-    remarkPlugins: [[remarkBasePath, `${BASE}/`]],
+    remarkPlugins: [[remarkBasePath, BASE]],
   },
 });
